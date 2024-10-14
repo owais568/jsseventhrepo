42bd675f90fd8ecb02fee669197b79f772c6132b
owais568 <owaishassan697@gmail.com>
2024-10-14 21:09:25 +0500

    add files

---
+++ app.js
@@ -0,0 +1,12 @@
+var date = new Date()
+console.log(date)
+
+var days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+console.log(days[date.getDay()])
+
+var newDate = new Date()
+console.log(newDate)
+
+var month = new Date()
+console.log(month.getMonth())
